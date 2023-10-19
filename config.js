0f3fb8e8d3627a1e775522936c10ae2307d0c678
eniskalyon <130925286+eniskalyon@users.noreply.github.com>
2023-10-19 15:28:45 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    consumer_key: 'DwpuXpLLjtmHx89Sr5xJd1yfw',
+    consumer_secret: 'pGVMiX81HHJUNFvyDSZM8rH7RY4mvQJIWnRUAMPrBW0HlKYuFj',
+    access_token: '1604453912781660160-bzHs7iF3IHM1COcqIdrg3ZeGb2wYNc',
+    access_token_secret: 'P22o6uKmiKOfu56DURQGTLn8bSWsJFphzGgBPCsBdsAt3'
+  }
+  
+  module.exports = config;
---
+++ images.js
@@ -0,0 +1,14 @@
+const images = [
+    {
+        file: "resadsorgul.jpg",
+        text: "Source: https://source.com/image1",
+        altText: "this is r's pezkovi"
+    },
+    {
+        file: "yucelalbayrak.jpg",
+        text: "Source: https://source.com/image2",
+        altText: "this is y's pezkovi"
+    }
+];
+
+module.exports = images;
